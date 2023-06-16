db44c465f4da5fd247fe95aaf70776d849dd1364
Marion Poirie <marionpoirie1996@gmail.com>
2023-06-16 14:46:31 +0200

    Finished Day One

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'bookmarks/destroy'
+  # get 'lists/index'
+  # get 'lists/new'
+  # get 'lists/create'
+  # get 'lists/show'
+  # get 'list/index'
+  # get 'list/new'
+  # get 'list/create'
+  # get 'list/show'
+  # get 'list/destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
